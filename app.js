59efa087aaf2cf6892d6e2141a038739ff64d67d
Fortunato <fortunato.maycotte@gmail.com>
2017-04-04 13:33:03 -0700

    Finished the 5 questions

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var sports = prompt('Do you like sports?');
 if (sports.trim().toLowerCase() == 'yes') {
-  alert('You were correct');
+  alert('Me too, Steelers is my football team');
 }
 else {
   alert('Sports are fun, you should try one sometime!');
@@ -13,4 +13,18 @@ if (work.trim().toLowerCase == 'yes') {
 }
 else {
   alert('Working is good for you, you should probably try it');
+}
+var drink = prompt('Do you like beer?');
+if (drink.trim().toLowerCase() == 'yes') {
+  alert('I also like beer, specially crafter beer');
+}
+else {
+  alert('Beer is good for the soul, lets have a beer some day.');
+}
+var color = prompt('Do you like the black color?');
+if (color.trim().toLowerCase() == 'yes') {
+  alert('Nice, it is an awesome color');
+}
+else {
+  alert('I guess color black is not for everybody');
 }
