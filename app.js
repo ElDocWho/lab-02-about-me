4730bfb944401dd8743f35d4ecf3742780a89d36
Fortunato <fortunato.maycotte@gmail.com>
2017-04-04 13:17:18 -0700

    Started working on questions

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var sports = prompt('Do you like sports?');
+if (sports.trim().toLowerCase() == 'yes') {
+  alert('You were correct');
+}
+else {
+  alert('Sports are fun, you should try one sometime!');
+}
+var work = prompt('Do you like working?');
+if (work.trim().toLowerCase == 'yes') {
+  alert('Good, I also like working');
+}
+else {
+  alert('Working is good for you, you should probably try it');
+}
