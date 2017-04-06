ac71c97b57da4682c04e6ca7f622bc56a9427863
Stephanie Dover <doverstephaniem@gmail.com>
2017-04-06 14:51:22 -0700

    function with for loops

--- app.js
+++ app.js
@@ -30,48 +30,61 @@ counter += questions(q4, 'no');
 counter += questions(q5, 'yes');
 
 
-  var guesNum;
-  var notGuessed = true;
-  var randomNumber = Math.floor(Math.random() * 10);
-  var guessState;
-  var states = ['Texas', 'Washington', 'Florida', 'Oklahoma', 'Idaho', 'Iowa'];
-  var stateNotGuessed = true;
+var guesNum;
+var notGuessed = true;
+var randomNumber = Math.floor(Math.random() * 10);
+var guessState;
+var states = ['Texas', 'Washington', 'Florida', 'Oklahoma', 'Idaho', 'Iowa'];
+var stateNotGuessed = true;
 
-  for (var i = 4;(i > 0) && (notGuessed === true); i--) {
-    var guessNum = prompt('\t Guess a number between 0 and 10 \n \t You have ' + i + ' opportunities left');
-    if (parseInt(guessNum) === randomNumber) {
-      alert('You got it!!!');
-      notGuessed = false;
+function guessingGame(){
+  var functionCounter;
+for (var i = 4;(i > 0) && (notGuessed === true); i--) {
+  var guessNum = prompt('\t Guess a number between 0 and 10 \n \t You have ' + i + ' opportunities left');
+  if (parseInt(guessNum) === randomNumber) {
+    alert('You got it!!!');
+    notGuessed = false;
+    functionCounter = 1;
+    console.log(counter);
+  } else if (parseInt(guessNum) < randomNumber) {
+    alert('Number too low!');
+  } else if (parseInt(guessNum) > randomNumber) {
+    alert('Number too high!');
+  }
+};
+if (notGuessed === true) {
+  alert('Better luck next time, you did not guess the number');
+  functionCounter = 0;
+};
+return functionCounter;
+};
+counter += guessingGame();
+
+function stateGuessingGame() {
+  var functionCounter;
+for (var i = 6; (i > 0) && (stateNotGuessed === true); i--) {
+  var guessState = prompt('Enter a state that I have lived in, opportunities left: ' + i);
+  for (var index = 0;(index < 6) && (stateNotGuessed === true);index++) {
+    if (guessState.trim().toLowerCase() === states[index].trim().toLowerCase()) {
+      alert('You got it!!!! the states I have lived in are: ' + states);
+      stateNotGuessed = false;
       counter ++;
       console.log(counter);
-    } else if (parseInt(guessNum) < randomNumber) {
-      alert('Number too low!');
-    } else if (parseInt(guessNum) > randomNumber) {
-      alert('Number too high!');
-    }
-  };
-  if (notGuessed === true) {
-    alert('Better luck next time, you did not guess the number');
-  };
-
-  for (var i = 6; (i > 0) && (stateNotGuessed === true); i--) {
-    var guessState = prompt('Enter a state that I have lived in, opportunities left: ' + i);
-    for (var index = 0;(index < 6) && (stateNotGuessed === true);index++) {
-      if (guessState.trim().toLowerCase() === states[index].trim().toLowerCase()) {
-        alert('You got it!!!! the states I have lived in are: ' + states);
-        stateNotGuessed = false;
-        counter ++;
-        console.log(counter);
-        console.log(states[index]);
-      }
-    }
-    if (stateNotGuessed === true) {
-      alert('Not in that state!');
+      console.log(states[index]);
+      functionCounter = 1;
     }
   }
-
   if (stateNotGuessed === true) {
-    alert('Sorry you did not guess any of the states, \n the states I have lived in are: ' + states);
+    alert('Not in that state!');
   }
-  */
-  alert (name + ' You got ' + counter + ' out of 7 right!');
+}
+
+if (stateNotGuessed === true) {
+  alert('Sorry you did not guess any of the states, \n the states I have lived in are: ' + states);
+  functionCounter = 0;
+}
+return functionCounter;
+};
+
+counter += stateGuessingGame();
+alert (name + ' You got ' + counter + ' out of 7 right!');
