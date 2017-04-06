6c6cf0722a651ee87166b2c5c4138a578aa282fb
Fortunato <fortunato.maycotte@gmail.com>
2017-04-06 06:10:54 -0700

    Finished the count variable

--- app.js
+++ app.js
@@ -1,45 +1,55 @@
 'use strict';
 
-var counter;
+var counter = 0;
+var name = prompt('What is your first name?');
 var sports = prompt('Do I like sports?');
 if (sports.trim().toLowerCase() == 'yes' || sports.trim().toLowerCase() == 'y') {
   alert('Correct!!! I do like sports');
   counter ++;
+  console.log(counter);
 }
 else {
   alert('Wrong!!! I do like sports');
 }
-console.log('Do you like sports? ' + sports);
+console.log('Do I like sports? ' + sports);
 var work = prompt('Do I like working?');
-if (work.trim().toLowerCase == 'yes' || work.trim().toLowerCase == 'y') {
+if (work.trim().toLowerCase == 'yes' || work.trim().toLowerCase() == 'y') {
   alert('Correct!!! I do like working');
   counter ++;
+  console.log(counter);
 }
 else {
   alert('Wrong!!! I do like working.');
 }
-console.log('Do you like working? ' + work);
+console.log('Do I like working? ' + work);
 var drink = prompt('Do I like beer? ');
 if (drink.trim().toLowerCase() == 'yes' || drink.trim().toLowerCase() == 'y') {
   alert('Correct!!! I like beer.');
+  counter ++;
+  console.log(counter);
 }
 else {
   alert('Wrong!!! I like beer');
-}console.log('Do you like beer?' + drink);
-var color = prompt('Do you like the black color? ');
+}
+console.log('Do I like beer? ' + drink);
+var color = prompt('Do I like the pink color? ');
 if (color.trim().toLowerCase() == 'yes' || color.trim().toLowerCase() == 'y') {
-  alert('Nice, it is an awesome color');
+  alert('Wrong!!! I do not like it.');
 }
 else {
-  alert('I guess color black is not for everybody');
+  alert('Correct, I don\t like pink, but I do like black.');
+  counter ++;
+  console.log(counter);
 }
-console.log('Do you like the black color? ' + color);
-var pizza = prompt('Do you like pizza? ');
+console.log('Do I like the pink color? ' + color);
+var pizza = prompt('Do I like pizza?');
 if (pizza.trim().toLowerCase() == 'yes' || pizza.trim().toLowerCase() == 'y') {
-  alert('Yeah!!! pizza is good');
+  alert('Correct!!! I do like pizza.');
+  counter ++;
+  console.log(counter);
 }
 else {
-  alert('Afraid of gaining weight?');
+  alert('Wrong!!! I do like pizza.');
 }
 console.log('Do you like pizza? ' + pizza);
 
@@ -55,6 +65,8 @@ for (var i = 4;(i > 0) && (notGuessed === true); i--) {
   if (parseInt(guessNum) === randomNumber) {
     alert('You got it!!!');
     notGuessed = false;
+    counter ++;
+    console.log(counter);
   } else if (parseInt(guessNum) < randomNumber) {
     alert('Number too low!');
   } else if (parseInt(guessNum) > randomNumber) {
@@ -69,9 +81,11 @@ for (var i = 6; (i > 0) && (stateNotGuessed === true); i--) {
   var guessState = prompt('Enter a state that I have lived in, opportunities left: ' + i);
   for (var index = 0;(index < 6) && (stateNotGuessed === true);index++) {
     if (guessState.trim().toLowerCase() === states[index].trim().toLowerCase()) {
-      console.log(states[index]);
       alert('You got it!!!! the states I have lived in are: ' + states);
       stateNotGuessed = false;
+      counter ++;
+      console.log(counter);
+      console.log(states[index]);
     }
   }
   if (stateNotGuessed === true) {
@@ -82,3 +96,4 @@ for (var i = 6; (i > 0) && (stateNotGuessed === true); i--) {
 if (stateNotGuessed === true) {
   alert('Sorry you did not guess any of the states, \n the states I have lived in are: ' + states);
 }
+alert (name + ' You got ' + count + 'out of 9 right!');
